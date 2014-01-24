50a287af2f94df3fc2eb84224280c50a3069d772
ravi-juhomi <ravi@juhomi.com>
2014-01-24 13:53:57 +0530

    table_name change

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,29 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user_registration (
+  id                        bigint not null,
+  full_name                 varchar(255),
+  company                   varchar(255),
+  email                     varchar(255),
+  pass_word                 varchar(255),
+  constraint pk_energy_link_registration primary key (id))
+;
+
+create sequence energy_link_registration_seq;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists energy_link_registration;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists energy_link_registration_seq;
+
